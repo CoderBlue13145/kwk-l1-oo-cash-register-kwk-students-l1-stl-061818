abba043a405cc4ea8e17c80c0b513a2df04f77f1
CoderBlue13145 <coderblue13145@gmail.com>
2018-06-22 20:07:47 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -2,7 +2,44 @@
 
 class User
 
-def initialize(username,password)
+def initialize(username,password,email)
+@username = username
+@password = password
+@email = email
+
+end
+
+def setUsername = (username)
+@username = username
+
+end
+
+def getUsername = (username)
+@username 
+
+end
+
+def setPassword = (password)
+@username = username
+
+end
+
+def getUsername = (username)
+@username 
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
